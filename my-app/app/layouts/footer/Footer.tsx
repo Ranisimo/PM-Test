1a110dcc34120fe8164582c5e983242abd5144ca
kizzysinar <kizzysinar@gmail.com>
2025-12-05 16:03:26 +0000

    updated footer css to better align with rest of project

--- my-app/app/layouts/footer/Footer.tsx
+++ my-app/app/layouts/footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer df-fc-ac-jc">
-      <div className="red-border"></div>
+      <div className="footer-red-border"></div>
       <div className="pay-options-container df-ac-jc">
         <div className="pay-options df-ac-jc">
           <img className="pay-options-img" src="/paypal.png"
@@ -19,17 +19,17 @@ export default function Footer() {
       <div className="quick-info">
         <div className="about-info">
           <img className="about-info-logo" src="/logoLight.svg" alt="Main logo" />
-          <p className="about-text urbanist-400">CFDs are complex instruments and come with a high risk of losing money rapidly due to leverage. 21.38% of retail investor accounts generate profits when trading CFDs with this provider. You should consider whether you understand how CFDs work and whether you can afford to take the high risk of losing your money.</p>
+          <p className="about-text">CFDs are complex instruments and come with a high risk of losing money rapidly due to leverage. 21.38% of retail investor accounts generate profits when trading CFDs with this provider. You should consider whether you understand how CFDs work and whether you can afford to take the high risk of losing your money.</p>
           <img className="about-info-socials" src="/socials.svg" alt="Main logo" />
         </div>
         <div className="links-container">
           <div>
             <p className="links-header">Quick links</p>
             <ul>
-              <li className="urbanist-400">About Us</li>
-              <li className="urbanist-400">Our Markets</li>
-              <li className="urbanist-400">Platforms</li>
-              <li className="urbanist-400">Pricing</li>
+              <li>About Us</li>
+              <li>Our Markets</li>
+              <li>Platforms</li>
+              <li>Pricing</li>
             </ul>
           </div>
           <div>
@@ -53,8 +53,8 @@ export default function Footer() {
         </div>
       </div>
       <div className="footer-warning df-fc-ac-jc">
-        <p className="urbanist-400">CFDs are complex instruments and come with a high risk of losing money rapidly due to leverage. 21.38% of retail investor accounts generate profits when trading CFDs with this provider. You should consider whether you understand how CFDs work and whether you can afford to take the high risk of losing your money.</p>
-        <p className="urbanist-400">Premier Markets Ltd, trading as Premier Markets is a company incorporated under company number: A000003075 in Anguilla. Our registered office is located at Anguilla, The Valley, Kismet House, Sandy Hill, PO Box P.O.Box 174The information on this site is not directed at residents of Russia, Afghanistan, Belgium, Canada, India, United States or any country or jurisdiction where such distribution or use would be contrary to local law, sanctions or regulation.</p>
+        <p className="footer-warning-text">CFDs are complex instruments and come with a high risk of losing money rapidly due to leverage. 21.38% of retail investor accounts generate profits when trading CFDs with this provider. You should consider whether you understand how CFDs work and whether you can afford to take the high risk of losing your money.</p>
+        <p className="footer-warning-text">Premier Markets Ltd, trading as Premier Markets is a company incorporated under company number: A000003075 in Anguilla. Our registered office is located at Anguilla, The Valley, Kismet House, Sandy Hill, PO Box P.O.Box 174The information on this site is not directed at residents of Russia, Afghanistan, Belgium, Canada, India, United States or any country or jurisdiction where such distribution or use would be contrary to local law, sanctions or regulation.</p>
       </div>
       <div className="footer-copyright df-ac-jc">
         Copyright © 2025 Premier Markets - All Rights Reserved.
